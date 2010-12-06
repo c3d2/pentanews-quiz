95fbd1e7d25f987b92902c994afe329bade1708f
Astro <astro@spaceboyz.net>
2010-12-06 19:19:52 +0100

    reconnecting websockets

--- server.js
+++ server.js
@@ -5,26 +5,32 @@ var wsc = require('websocket-client');
 var frontend;
 
 /* TODO: url */
-var nedap = new wsc.WebSocket('ws://localhost:8080/', 'quiz-nedap');
-nedap.onopen = function() {
-    console.log('NEDAP opened');
-    nedap.send('nedap-kneemFothbedchoadHietEnobKavLub1');
-};
-nedap.onclose = function() {
-    console.log('NEDAP closed');
-};
-nedap.onerror = function(e) {
-    console.log('NEDAP error: ' + e.message);
-};
-nedap.onmessage = function(data) {
-    try {
-	var msg = JSON.parse(data);
-	console.log({ fromNedap: msg });
-	frontend.send(JSON.stringify({ nedap: msg }));
-    } catch (e) {
-	console.error(e.stack);
-    }
-};
+var nedap;
+function connectNedap() {
+    nedap = new wsc.WebSocket('ws://localhost:8080/', 'quiz-nedap');
+    nedap.onopen = function() {
+	console.log('NEDAP opened');
+	nedap.send('nedap-kneemFothbedchoadHietEnobKavLub1');
+    };
+    nedap.onclose = function() {
+	console.log('NEDAP closed');
+	connectNedap();
+    };
+    nedap.onerror = function(e) {
+	console.log('NEDAP error: ' + e.message);
+	connectNedap();
+    };
+    nedap.onmessage = function(data) {
+	try {
+	    var msg = JSON.parse(data);
+	    console.log({ fromNedap: msg });
+	    frontend.send(JSON.stringify({ nedap: msg }));
+	} catch (e) {
+	    console.error(e.stack);
+	}
+    };
+}
+connectNedap();
 
 var server = Connect.createServer(
     Connect.logger(),
