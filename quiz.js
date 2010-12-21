91a57d03783ad5d3571156418d3df0919dee60d2
Astro <astro@spaceboyz.net>
2010-12-21 22:48:11 +0100

    don't display audience & phone jokers

--- quiz.js
+++ quiz.js
@@ -141,7 +141,7 @@ function startQuiz() {
         if (name) {
             playerNames[i] = name;
             playerScores[i] = 0;
-            $('#scoreboard dl').append('<dt></dt><dd><span class="score">0</span><img src="fiftyfifty.png" class="fiftyfifty"><img src="audience.png" class="audience"><img src="phone.png" class="phone"><img src="nedap.png" class="nedap"><img src="irc.png" class="irc"><img src="fwd.png" class="fwd"></dd>');
+            $('#scoreboard dl').append('<dt></dt><dd><span class="score">0</span><img src="fiftyfifty.png" class="fiftyfifty"><img src="nedap.png" class="nedap"><img src="irc.png" class="irc"><img src="fwd.png" class="fwd"></dd>');
             $('#scoreboard dl dt').last().text(name);
             $('#players').append('<li class="player'+i+'"><span class="name"></span><span class="score">0</span></li>');
             $('#players li.player'+i+' span.name').text(name);
@@ -217,7 +217,7 @@ function takeJoker(activePlayer, joker) {
 	sendToBackend({ nedap: { joker: { question: q.text,
 					  answers: q.answers
 	} } });
-	
+
 	$('#nedap').show();
 	var scores = [0, 0, 0, 0];
 	var redraw = function() {
